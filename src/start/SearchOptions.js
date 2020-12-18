e7866ccc41be8910a6d9a2139711675cc4c6ab2f
Klara Eserstam <klara.eserstam@gmail.com>
2020-12-18 01:38:26 +0100

    Fix button css

--- src/start/SearchOptions.js
+++ src/start/SearchOptions.js
@@ -5,12 +5,16 @@ import './SearchOptions.css'
 function SearchOptions() {
   return (
     <div className="optionsContainer">
+      {/* <div className="option-left"> */}
       <Link to="/search/city">
         <TextButton text="Search by City"></TextButton>
       </Link>
+      {/* </div> */}
+      {/* <div className="option-right"> */}
       <Link to="/search/country">
         <TextButton text="Search by Country"></TextButton>
       </Link>
+      {/* </div> */}
     </div>
   )
 }
