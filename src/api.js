b40ff5a8e6a92d741f986405e7ae38ab130a376e
Klara Eserstam <klara.eserstam@gmail.com>
2020-12-18 21:09:28 +0100

    Fix api to query cities by country with country code

--- src/api.js
+++ src/api.js
@@ -1,16 +1,31 @@
 const API_USERNAME = process.env.REACT_APP_API_USERNAME
-const createUrl = (query, numberOfResults, orderby) =>
-  `http://api.geonames.org/search?q=${query}&featureCode=PPLC&featureCode=PPLA` +
-  `&maxRows=${numberOfResults}&orderby=${orderby}&style=long&type=json&username=${API_USERNAME}`
+const baseUrl = `http://api.geonames.org/search?fuzzy=0.8&style=long&type=json&username=${API_USERNAME}`
+const cityFeatureCodes = `featureCode=PPLC&featureCode=PPLA`
 
 export const getCity = async (query) => {
-  const url = createUrl(query, 1, 'relevance')
-  return fetch(url).then((res) => processResponse(res))
+  const url = `${baseUrl}&name=${query}&${cityFeatureCodes}&orderby=relevance&maxRows=1`
+  return fetch(url).then(processResponse)
 }
 
 export const getCitiesByCountry = async (query, numberOfCities) => {
-  const url = createUrl(query, numberOfCities, 'population')
-  return fetch(url).then((res) => processResponse(res))
+  const countryFeatureCodes = `featureCode=${[
+    'PCL',
+    'PCLD',
+    'PCLF',
+    'PCLH',
+    'PCLI',
+    'PCLIX',
+    'PCLS',
+  ].join('&featureCode=')}`
+
+  // Get country code for queried country
+  const countryQueryUrl = `${baseUrl}&name=${query}&${countryFeatureCodes}&orderby=relevance&maxRows=1&fuzzy=0.8`
+  const countryRes = await fetch(countryQueryUrl).then(processResponse)
+  const countryCode = countryRes.geonames[0].countryCode
+
+  // Fetch cities based on country code
+  const citiesQueryUrl = `${baseUrl}&name=${query}&country=${countryCode}&${cityFeatureCodes}&orderby=population&maxRows=${numberOfCities}`
+  return fetch(citiesQueryUrl).then(processResponse)
 }
 
 const processResponse = async (res) => {
